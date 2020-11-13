c6b5b9a188213779796e66113bbe825558444a20
maguilarac <maguilarac@NB8134.mshome.net>
2020-11-13 00:37:28 -0500

    3. Ciclo de vida de pruebas , @Disabled

--- app/src/test/java/pe/com/arland/cliente1/registro/AppTest.java
+++ app/src/test/java/pe/com/arland/cliente1/registro/AppTest.java
@@ -11,15 +11,68 @@ import pe.com.arland.cliente1.registro.entity.factory.IFabricaVehiculo;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+
 class AppTest {
+	
+	/* Intanciando la aplicacion principal
     @Test void appHasAGreeting() {
       App classUnderTest = new App();
       assertNotNull(classUnderTest.getGreeting(), "app should have a greeting");
-    }
+    }*/
     
-    
-    @Test 
-    void mySimpleTest () { 
-          // Mi lógica de prueba aquí 
-    } 
+	@BeforeAll
+	static void setupAll() {
+		System.out.println("****************************************");
+		System.out.println("*ETAPA :BEFFOREALL                  ******");
+		System.out.println("Configurar TODAS LAS PRUEBAS en la clase");
+		System.out.println("****************************************");
+	}
+
+	@BeforeEach
+	void setup() {
+		System.out.println("****************************************");
+		System.out.println(">>ETAPA :BEFORE EACH");
+		System.out.println("Configurar ANTES CADA PRUEBA en la clase");
+		System.out.println("****************************************");
+	}
+
+	@Test
+	void testOne() {
+		System.out.println("TEST 1");
+	}
+
+	@Disabled
+	@Test
+	void testTwo() {
+		System.out.println("TEST 2");
+	}
+	
+	
+	@Test
+	void testThree() {
+		System.out.println("TEST 3");
+	}
+
+	@AfterEach
+	void teardown() {
+		System.out.println("Teardown EACH TEST in the class");
+		System.out.println("****************************************");
+		System.out.println(">>ETAPA :AFTEREACH ");
+		System.out.println("Configurar LUEGO DE CADA PRUEBA en la clase");
+		System.out.println("****************************************");
+	}
+
+	@AfterAll
+	static void teardownAll() {
+		System.out.println("****************************************");
+		System.out.println("*ETAPA :AFTERPALL                  ******");
+		System.out.println("Desmontando  TODAS LAS PRUEBAS en la clase");
+		System.out.println("****************************************");
+	}
+  
 }
